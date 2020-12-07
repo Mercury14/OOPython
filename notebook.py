de9dc3acb3ccd415d1c5a6fcc389d49a82bc7fe6
SimonB <bartlett.se@googlemail.com>
2020-12-07 08:49:20 +0000

    Added menu class

--- notebook.py
+++ notebook.py
@@ -38,13 +38,21 @@ class Notebook:
         '''Create a new note and add it to the list.'''
         self.notes.append(Note(memo, tags))
 
+    def _find_note(self, note_id):
+        '''Locate the note with the given id.'''
+        for note in self.notes:
+            if str(note.id) == str(note_id):
+                return note
+            return None
+
     def modify_memo(self, note_id, memo):
         '''Find the note with the given id and change
         its memo to the given value.'''
-        for note in self.notes:
-            if note.id == note_id:
-                note.memo = memo
-                break
+        note = self._find_note(note_id)
+        if note:
+            note.memo = memo
+            return True
+        return False
 
     def modify_tags(self, note_id, tags):
         '''Find the note with the given id and change its
